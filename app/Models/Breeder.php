f6833b282281a4993376f42552db4f66b9cca485
Niiph <pepo.o@wp.pl>
2021-11-24 17:48:33 +0100

    .

--- app/Models/Breeder.php
+++ app/Models/Breeder.php
@@ -20,6 +20,7 @@ class Breeder extends Model
         return $this->hasMany(Plant::class);
     }
 
+    //i was guessing here, do not know if thats good guess because I am unable to recall it into my view
     public function groups()
     {
         return $this->belongsTo(Group::class);
